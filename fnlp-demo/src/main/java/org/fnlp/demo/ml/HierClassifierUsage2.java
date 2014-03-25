455eb23e9e65fd0edd80e7c04dfa4558af88e3bd
xpqiu <xpqiu@fudan.edu.cn>
2014-03-25 15:04:45 +0800

    add demo code

---
+++ fnlp-demo/src/main/java/org/fnlp/demo/ml/HierClassifierUsage2.java
@@ -0,0 +1,82 @@
+package org.fnlp.demo.ml;
+
+
+import java.io.File;
+
+import org.fnlp.data.reader.SimpleFileReader;
+import org.fnlp.ml.classifier.hier.Linear;
+import org.fnlp.ml.classifier.hier.PATrainer;
+import org.fnlp.ml.classifier.hier.inf.MultiLinearMax;
+import org.fnlp.ml.classifier.linear.inf.Inferencer;
+import org.fnlp.ml.feature.BaseGenerator;
+import org.fnlp.ml.loss.ZeroOneLoss;
+import org.fnlp.ml.types.InstanceSet;
+import org.fnlp.ml.types.alphabet.AlphabetFactory;
+import org.fnlp.ml.types.alphabet.IFeatureAlphabet;
+import org.fnlp.ml.types.alphabet.LabelAlphabet;
+import org.fnlp.nlp.pipe.Pipe;
+import org.fnlp.nlp.pipe.SeriesPipes;
+import org.fnlp.nlp.pipe.StringArray2SV;
+import org.fnlp.nlp.pipe.Target2Label;
+
+/**
+ * 层次分类器使用示例
+ * 
+ * @author xpqiu
+ * 
+ */
+public class HierClassifierUsage2 {
+	static InstanceSet train;
+	static InstanceSet test;
+	static AlphabetFactory factory = AlphabetFactory.buildFactory();
+	static LabelAlphabet al = factory.DefaultLabelAlphabet();
+	static IFeatureAlphabet af = factory.DefaultFeatureAlphabet();
+	static String path = null;
+
+	public static void main(String[] args) throws Exception {
+
+		
+		long start = System.currentTimeMillis();
+
+		path = "./example-data/data-classification.txt";
+
+		Pipe lpipe = new Target2Label(al);
+		Pipe fpipe = new StringArray2SV(factory, true);
+		//构造转换器组
+		Pipe pipe = new SeriesPipes(new Pipe[]{lpipe,fpipe});
+		
+		//构建训练集
+		train = new InstanceSet(pipe, factory);
+		SimpleFileReader reader = new SimpleFileReader (path,true);
+		train.loadThruStagePipes(reader);
+		al.setStopIncrement(true);
+		
+		//构建测试集
+		test = new InstanceSet(pipe, factory);		
+		reader = new SimpleFileReader (path,true);
+		test.loadThruStagePipes(reader);	
+
+		System.out.println("Train Number: " + train.size());
+		System.out.println("Test Number: " + test.size());
+		System.out.println("Class Number: " + al.size());
+
+		float c = 1.0f;
+		int round = 20;
+		
+		BaseGenerator featureGen = new BaseGenerator();
+		ZeroOneLoss loss = new ZeroOneLoss();
+		Inferencer msolver = new MultiLinearMax(featureGen, al, null,2);
+
+		PATrainer trainer = new PATrainer(msolver, featureGen, loss, round,c, null);
+		Linear pclassifier = trainer.train(train, null);
+		
+		String modelFile = path+".m.gz";
+		pclassifier.saveTo(modelFile);
+
+		long end = System.currentTimeMillis();
+		System.out.println("Total Time: " + (end - start));
+		System.out.println("End!");
+		(new File(modelFile)).deleteOnExit();
+		System.exit(0);
+	}
+}
---
+++ fnlp-demo/src/main/java/org/fnlp/demo/ml/SimpleClassifier2.java
@@ -0,0 +1,87 @@
+package org.fnlp.demo.ml;
+
+
+import java.io.File;
+
+import org.fnlp.data.reader.SimpleFileReader;
+import org.fnlp.ml.classifier.linear.Linear;
+import org.fnlp.ml.classifier.linear.OnlineTrainer;
+import org.fnlp.ml.classifier.linear.inf.Inferencer;
+import org.fnlp.ml.classifier.linear.inf.LinearMax;
+import org.fnlp.ml.classifier.linear.update.LinearMaxPAUpdate;
+import org.fnlp.ml.feature.Generator;
+import org.fnlp.ml.feature.SFGenerator;
+import org.fnlp.ml.loss.ZeroOneLoss;
+import org.fnlp.ml.types.InstanceSet;
+import org.fnlp.ml.types.alphabet.AlphabetFactory;
+import org.fnlp.ml.types.alphabet.IFeatureAlphabet;
+import org.fnlp.ml.types.alphabet.LabelAlphabet;
+import org.fnlp.nlp.pipe.StringArray2IndexArray;
+import org.fnlp.nlp.pipe.Pipe;
+import org.fnlp.nlp.pipe.SeriesPipes;
+import org.fnlp.nlp.pipe.Target2Label;
+
+/**
+ * 线性分类器使用示例
+ * 
+ * @author xpqiu
+ * 
+ */
+public class SimpleClassifier2 {
+	static InstanceSet train;
+	static InstanceSet test;
+	static AlphabetFactory factory = AlphabetFactory.buildFactory();
+	static LabelAlphabet al = factory.DefaultLabelAlphabet();
+	static IFeatureAlphabet af = factory.DefaultFeatureAlphabet();
+	static String path = null;
+
+	public static void main(String[] args) throws Exception {
+
+		
+		long start = System.currentTimeMillis();
+
+		path = "./example-data/data-classification.txt";
+
+		Pipe lpipe = new Target2Label(al);
+		Pipe fpipe = new StringArray2IndexArray(factory, true);
+		//构造转换器组
+		Pipe pipe = new SeriesPipes(new Pipe[]{lpipe,fpipe});
+		
+		//构建训练集
+		train = new InstanceSet(pipe, factory);
+		SimpleFileReader reader = new SimpleFileReader (path,true);
+		train.loadThruStagePipes(reader);
+		al.setStopIncrement(true);
+		
+		//构建测试集
+		test = new InstanceSet(pipe, factory);		
+		reader = new SimpleFileReader (path,true);
+		test.loadThruStagePipes(reader);	
+
+		System.out.println("Train Number: " + train.size());
+		System.out.println("Test Number: " + test.size());
+		System.out.println("Class Number: " + al.size());
+
+		float c = 1.0f;
+		int round = 20;
+		
+		Generator featureGen = new SFGenerator();
+		ZeroOneLoss loss = new ZeroOneLoss();
+		LinearMaxPAUpdate update = new LinearMaxPAUpdate(loss);
+		
+		
+		Inferencer msolver = new LinearMax(featureGen, al.size() );
+		OnlineTrainer trainer = new OnlineTrainer(msolver, update, loss, af.size(), round,
+				c);
+
+		Linear classify = trainer.train(train, test);
+		String modelFile = path+".m.gz";
+		classify.saveTo(modelFile);
+
+		long end = System.currentTimeMillis();
+		System.out.println("Total Time: " + (end - start));
+		System.out.println("End!");
+		(new File(modelFile)).deleteOnExit();
+		System.exit(0);
+	}
+}
